c2da1b5aa56a686c266ec429c050ab3ede586087
wmancaniello <wmancaniello@gmail.com>
2024-05-29 19:02:41 +0200

    add: query 8

--- query.sql
+++ query.sql
@@ -26,4 +26,8 @@ SELECT * FROM `degrees` WHERE `level` = "magistrale";
 SELECT COUNT(*) FROM `departments`;
 
 
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT COUNT(*) FROM `teachers` WHERE `phone` IS NULL;
+
+
 
