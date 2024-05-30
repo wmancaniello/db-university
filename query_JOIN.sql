29c21465d7cf05b556c95b389f13ea3aeee5c51e
wmancaniello <wmancaniello@gmail.com>
2024-05-30 19:02:21 +0200

    add: query 3 and 4

--- query_JOIN.sql
+++ query_JOIN.sql
@@ -21,3 +21,29 @@ FROM
 INNER JOIN `departments` ON `degrees`.`department_id` = `department_id`
 WHERE
     `departments`.`name` = "Dipartimento di Neuroscienze" AND `degrees`.`level` = "magistrale";
+
+-- 3.  Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT
+    `courses`.`name` AS `Corsi Professore`
+FROM
+    `courses`
+INNER JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE
+    `course_teacher`.`teacher_id` = 44;
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT
+    `students`.`name` AS `Nome`,
+    `students`.`surname` AS `Cognome`,
+    `degrees`.`name` AS `Corso di laurea`,
+    `departments`.`name` AS `Dipartimento`
+FROM
+    `students`
+INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY
+    `students`.`surname`,
+    `students`.`name`;
